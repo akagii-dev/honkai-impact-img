f7d1dda44a09e64d0027e10eae3816880544503e
muffin1000 <97124098+muffin1000@users.noreply.github.com>
2022-08-10 02:33:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-import cv2
-import requests
-import tempfile
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-import datetime
+from fake_useragent import UserAgent
+import time, os, datetime, urllib.request
 
 options = Options()
 url = "https://honkaiimpact3.hoyoverse.com/global/en-us/media/wallpaper"
 options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(),  chrome_options=options)
+ua = UserAgent(verify_ssl=False)
+userAgent = ua.random
+options.add_argument(f'user-agent={userAgent}')
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.set_window_size('1200', '1000')
 driver.get(url)
 
-def get_image_url():
+def getImg():
     el = driver.find_elements(By.CLASS_NAME, "img-container")
-    for i in el:
-        imgurl = i.find_element(By.TAG_NAME, "img").get_attribute("src").split("?")[-2]
-        imgre = requests.get(imgurl)
-        fp = tempfile.NamedTemporaryFile(dir='./img', delete=False)
-        fp.write(imgre.content)
-        img = cv2.imread(fp.name)
-        fp.close()
-        fpname = "./img/" + datetime.datetime.now().strftime("%H-%M-%S") + ".png"
-        cv2.imwrite(fpname, img)
-        os.remove(fp.name)
+    for j in el:
+        imgurl = j.find_element(By.TAG_NAME, "img").get_attribute("src").split("?")[-2]
+        savetime = time.strftime("%d%H%M%S", time.localtime())
+        urllib.request.urlretrieve(imgurl, "./img/"+savetime+".jpg")
+        time.sleep(0.5)
 
-for j in range(29):
-    get_image_url()
+count = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div[3]/div[2]/ul/li[15]/a").text    
+count = int(count)
+for i in range(count):
+    getImg()
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
     driver.find_element(By.CLASS_NAME, "next-btn").find_element(By.TAG_NAME, "a").click()
